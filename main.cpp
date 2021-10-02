dbeb6afafd3f237dcd1d60a8db129d003af3416b
chboo1 <samuel.navert@gmail.com>
2021-10-02 16:46:09 -0400

    Added movement (also stupid player model)

--- main.cpp
+++ main.cpp
@@ -12,15 +12,22 @@ class Main
     bool init();
     bool loadGraphics();
     bool loadAudio();
+    bool initRects();
     void gameloop();
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
+    SDL_Surface* p = NULL;
     SDL_Event e;
     SDL_Rect groundRect = {0, height - height / 100, width, height / 100};
+    SDL_Rect pRect;
     bool quit = false;
+    int hspeed = 0;
+    int vspeed = 0;
+    bool aPressed = false;
+    bool dPressed = false;
     Main()
     {
-      if (!init())
+      if (!init() || !loadGraphics() || !initRects())
       {
         std::cout << "Failed to initialize '~' :/" << std::endl;
       }
@@ -60,6 +67,26 @@ bool Main::init()
 }
 
 
+bool Main::loadGraphics()
+{
+  p = SDL_LoadBMP("graphics/frame_1.bmp");
+  bool success = true;
+  if (p == NULL)
+  {
+    success = false;
+  }
+  return true;
+}
+
+
+bool Main::initRects()
+{
+  pRect.x = 0;
+  pRect.y =  width - (width / 100 * 6);
+  return true;
+}
+
+
 void Main::gameloop()
 {
   while (SDL_PollEvent(&e) != 0)
@@ -73,13 +100,66 @@ void Main::gameloop()
       switch(e.key.keysym.sym)
       {
         case SDLK_ESCAPE:
-        quit = true;
-        break;
+          quit = true;
+          break;
+        case SDLK_a:
+          aPressed = true;
+          break;
+        case SDLK_d:
+          dPressed = true;
+          break;
+      }
+    }
+    else if (e.type == SDL_KEYUP)
+    {
+      switch(e.key.keysym.sym)
+      {
+        case SDLK_a:
+          aPressed = false;
+          break;
+        case SDLK_d:
+          dPressed = false;
+          break;
       }
     }
   }
+  if (dPressed)
+  {
+    if (hspeed < 5)
+    {
+      hspeed++;
+    }
+    if (aPressed)
+    {
+      hspeed = 0;
+    }
+  }
+  if (aPressed)
+  {
+    if (hspeed > -5)
+    {
+      hspeed--;
+    }
+    if (dPressed)
+    {
+      hspeed = 0;
+    }
+  }
+  if (!dPressed && !aPressed)
+  {
+    if (hspeed < 0)
+    {
+      hspeed++;
+    }
+    if (hspeed > 0)
+    {
+      hspeed--;
+    }
+  }
+  pRect.x += hspeed;
   SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
   SDL_FillRect(screenSurface, &groundRect, SDL_MapRGB(screenSurface->format,  0xA5, 0x4A, 0x2A));
+  SDL_BlitSurface(p, NULL, screenSurface, &pRect);
   SDL_UpdateWindowSurface(window);
 }
 
